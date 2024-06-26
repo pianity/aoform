d5691b1d0c5fc3b84505ce0c54110b7a3a38681a
Timothee Barran <timothee.barran@gmail.com>
2024-06-26 18:52:01 +0200

    Using localnet

--- src/deployProcesses.mjs
+++ src/deployProcesses.mjs
@@ -1,40 +1,47 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
-console.log('aoform v1.3')
+console.log("aoform v1.3");
 
-import yaml from 'js-yaml';
-import fs from 'fs';
-import crypto from 'crypto';
-import path from 'path';
-import { connect, createDataItemSigner } from '@permaweb/ao-connect';
+import yaml from "js-yaml";
+import fs from "fs";
+import crypto from "crypto";
+import path from "path";
+import { connect, createDataItemSigner } from "@permaweb/ao-connect";
 
 // Connect to the AO network
-const ao = connect();
+const ao = connect({
+  CU_URL: "http://localhost:4004",
+  GATEWAY_URL: "http://localhost:4000",
+  GRAPHQL_URL: "http://localhost:4000/graphql",
+  MU_URL: "http://localhost:4002",
+});
 
-console.log(ao)
+console.log(ao);
 
 // Load the YAML file
-const processesYamlPath = path.join(process.cwd(), 'processes.yaml');
+const processesYamlPath = path.join(process.cwd(), "processes.yaml");
 let processes = [];
 try {
-  const processesYaml = fs.readFileSync(processesYamlPath, 'utf8');
+  const processesYaml = fs.readFileSync(processesYamlPath, "utf8");
   processes = yaml.load(processesYaml);
 } catch (err) {
-  if (err.code !== 'ENOENT') {
+  if (err.code !== "ENOENT") {
     throw err;
   }
-  console.warn('processes.yaml file not found. No processes will be deployed or updated.');
+  console.warn(
+    "processes.yaml file not found. No processes will be deployed or updated."
+  );
 }
 
 // Load the state file or create a new one
 let state;
 try {
-  const stateYamlPath = path.join(process.cwd(), 'state.yaml');
-  const stateYaml = fs.readFileSync(stateYamlPath, 'utf8');
+  const stateYamlPath = path.join(process.cwd(), "state.yaml");
+  const stateYaml = fs.readFileSync(stateYamlPath, "utf8");
   state = yaml.load(stateYaml);
 } catch (err) {
-  if (err.code === 'ENOENT') {
+  if (err.code === "ENOENT") {
     state = {};
   } else {
     throw err;
@@ -44,142 +51,154 @@ try {
 // Function to get the hash of a file
 function getFileHash(filePath) {
   const fileBuffer = fs.readFileSync(filePath);
-  const hashSum = crypto.createHash('sha256');
+  const hashSum = crypto.createHash("sha256");
   hashSum.update(fileBuffer);
-  return hashSum.digest('hex');
+  return hashSum.digest("hex");
 }
 
 // Function to deploy a process
 async function deployProcess(processInfo) {
-    const name = processInfo.name;
-    const filePath = processInfo.file;
-    const tags = processInfo.tags || [];
-    const currentHash = getFileHash(filePath);
-    const prerunFilePath = processInfo.prerun || ''; // Get the prerun file path, or an empty string if not provided
-    
-    // Check if the process has already been deployed
-    if (state[name]) {
-      const processState = state[name];
-      const lastHash = processState.hash;
-  
-      if (lastHash === currentHash) {
-        console.log(`Process '${name}' is up-to-date.`);
-        return;
-      }
-    }
-  
-    // Load the Lua file
-    const mainScript = fs.readFileSync(filePath, 'utf8');
-  
-    // Load the prerun script, if provided
-    let prerunScript = '';
-    if (prerunFilePath) {
-      prerunScript = fs.readFileSync(prerunFilePath, 'utf8');
-    }
-  
-    // Concatenate the prerun script with the main script
-    const luaCode = `${prerunScript}\n${mainScript}`;
-  
-    if(!process.env.WALLET_JSON) {
-      console.error("Missing WALLET_JSON environment variable. Please provide the wallet JSON in the environment variable WALLET_JSON.");
-      process.exit(1);
-    }
+  const name = processInfo.name;
+  const filePath = processInfo.file;
+  const tags = processInfo.tags || [];
+  const currentHash = getFileHash(filePath);
+  const prerunFilePath = processInfo.prerun || ""; // Get the prerun file path, or an empty string if not provided
 
-    let processId;
-    const wallet = JSON.parse(process.env.WALLET_JSON); // Read wallet from environment variable
-    const signer = createDataItemSigner(wallet);
+  // Check if the process has already been deployed
+  if (state[name]) {
+    const processState = state[name];
+    const lastHash = processState.hash;
 
-    console.log("Spawning process...", {
-        module: processInfo.module,
-        scheduler: processInfo.scheduler,
-        signer,
-        tags,
-      })
-    if (!state[name] || !state[name].processId) {
-      let spawnAttempts = 0;
-      const maxSpawnAttempts = 5;
-      const spawnDelay = 30000; // 30 seconds
-      
-      while (spawnAttempts < maxSpawnAttempts) {
-        try {
-          processId = await ao.spawn({
-            module: processInfo.module,
-            scheduler: processInfo.scheduler,
-            signer: createDataItemSigner(wallet),
-            tags,
-          });
-          console.log("Spawned process:", processId);
-          break;
-        } catch (err) {
-          spawnAttempts++;
-          console.log('err', err)
-          console.log(`Failed to spawn process '${name}'. Attempt ${spawnAttempts}/${maxSpawnAttempts}`);
-          if (spawnAttempts === maxSpawnAttempts) {
-            console.error('error', err);
-            console.error(`Failed to spawn process '${name}' after ${maxSpawnAttempts} attempts.`);
-            process.exit(1)
-          } else {
-            console.log(`Retrying in ${spawnDelay / 1000} seconds...`);
-            await new Promise(resolve => setTimeout(resolve, spawnDelay));
-          }
-        }
-      }
-    } else {
-      processId = state[name].processId;
-      console.log(`Using existing process ID '${processId}' for process '${name}'.`);
+    if (lastHash === currentHash) {
+      console.log(`Process '${name}' is up-to-date.`);
+      return;
     }
-  
-    // Try sending the 'eval' action 5 times with a 30-second delay
-    let attempts = 0;
-    const maxAttempts = 5;
-    const delay = 30000; // 30 seconds
-  
-    console.log("Sending code...")
-    while (attempts < maxAttempts) {
+  }
+
+  // Load the Lua file
+  const mainScript = fs.readFileSync(filePath, "utf8");
+
+  // Load the prerun script, if provided
+  let prerunScript = "";
+  if (prerunFilePath) {
+    prerunScript = fs.readFileSync(prerunFilePath, "utf8");
+  }
+
+  // Concatenate the prerun script with the main script
+  const luaCode = `${prerunScript}\n${mainScript}`;
+
+  if (!process.env.WALLET_JSON) {
+    console.error(
+      "Missing WALLET_JSON environment variable. Please provide the wallet JSON in the environment variable WALLET_JSON."
+    );
+    process.exit(1);
+  }
+
+  let processId;
+  const wallet = JSON.parse(process.env.WALLET_JSON); // Read wallet from environment variable
+  const signer = createDataItemSigner(wallet);
+
+  console.log("Spawning process...", {
+    module: processInfo.module,
+    scheduler: processInfo.scheduler,
+    signer,
+    tags,
+  });
+  if (!state[name] || !state[name].processId) {
+    let spawnAttempts = 0;
+    const maxSpawnAttempts = 5;
+    const spawnDelay = 30000; // 30 seconds
+
+    while (spawnAttempts < maxSpawnAttempts) {
       try {
-        const r = await ao.message({ 
-          process: processId, 
-          data: luaCode,
-          tags: [
-            {
-              name: 'Action',
-              value: 'Eval'
-            }
-          ],
-          signer 
+        processId = await ao.spawn({
+          module: processInfo.module,
+          scheduler: processInfo.scheduler,
+          signer: createDataItemSigner(wallet),
+          tags,
         });
-        console.log(`Successfully sent 'eval' action for process '${name}'.`);
-        console.log(r);
+        console.log("Spawned process:", processId);
         break;
       } catch (err) {
-        attempts++;
-        console.error('error', err)
-
-        console.log(`Failed to send 'eval' action for process '${name}'. Attempt ${attempts}/${maxAttempts}`);
-        if (attempts === maxAttempts) {
-          console.error(`Failed to send 'eval' action for process '${name}' after ${maxAttempts} attempts.`);
+        spawnAttempts++;
+        console.log("err", err);
+        console.log(
+          `Failed to spawn process '${name}'. Attempt ${spawnAttempts}/${maxSpawnAttempts}`
+        );
+        if (spawnAttempts === maxSpawnAttempts) {
+          console.error("error", err);
+          console.error(
+            `Failed to spawn process '${name}' after ${maxSpawnAttempts} attempts.`
+          );
+          process.exit(1);
         } else {
-          console.log(`Retrying in ${delay / 1000} seconds...`);
-          await new Promise(resolve => setTimeout(resolve, delay));
+          console.log(`Retrying in ${spawnDelay / 1000} seconds...`);
+          await new Promise((resolve) => setTimeout(resolve, spawnDelay));
         }
       }
     }
-  
-    // Update the state
-    state[name] = {
-      processId,
-      hash: currentHash,
-    };
+  } else {
+    processId = state[name].processId;
+    console.log(
+      `Using existing process ID '${processId}' for process '${name}'.`
+    );
   }
 
-  export async function deployProcesses() {
-    // Deploy or update processes
-    for (const processInfo of processes) {
-      await deployProcess(processInfo);
+  // Try sending the 'eval' action 5 times with a 30-second delay
+  let attempts = 0;
+  const maxAttempts = 5;
+  const delay = 30000; // 30 seconds
+
+  console.log("Sending code...");
+  while (attempts < maxAttempts) {
+    try {
+      const r = await ao.message({
+        process: processId,
+        data: luaCode,
+        tags: [
+          {
+            name: "Action",
+            value: "Eval",
+          },
+        ],
+        signer,
+      });
+      console.log(`Successfully sent 'eval' action for process '${name}'.`);
+      console.log(r);
+      break;
+    } catch (err) {
+      attempts++;
+      console.error("error", err);
+
+      console.log(
+        `Failed to send 'eval' action for process '${name}'. Attempt ${attempts}/${maxAttempts}`
+      );
+      if (attempts === maxAttempts) {
+        console.error(
+          `Failed to send 'eval' action for process '${name}' after ${maxAttempts} attempts.`
+        );
+      } else {
+        console.log(`Retrying in ${delay / 1000} seconds...`);
+        await new Promise((resolve) => setTimeout(resolve, delay));
+      }
     }
-  
-    // Save the updated state
-    const updatedState = yaml.dump(state);
-    const stateYamlPath = path.join(process.cwd(), 'state.yaml');
-    fs.writeFileSync(stateYamlPath, updatedState, 'utf8');
   }
+
+  // Update the state
+  state[name] = {
+    processId,
+    hash: currentHash,
+  };
+}
+
+export async function deployProcesses() {
+  // Deploy or update processes
+  for (const processInfo of processes) {
+    await deployProcess(processInfo);
+  }
+
+  // Save the updated state
+  const updatedState = yaml.dump(state);
+  const stateYamlPath = path.join(process.cwd(), "state.yaml");
+  fs.writeFileSync(stateYamlPath, updatedState, "utf8");
+}
